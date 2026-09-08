80496ef841e7ca6ec1ae132fdfce9de50d00331c
pqthinhit <phamquangthinh21052005@gmail.com>
2026-09-08 19:25:26 +0700

    first commit

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -12,7 +12,7 @@ export const AuthContextProvider = ({ children }) => {
 
   const login = async (inputs) => {
     //TO DO
-    const res = axios.post("http://localhost:8800/api/auth/login",inputs,{
+    const res = axios.post("http://143.198.217.126:8800/api/auth/login",inputs,{
       withCredentials:true,
     });
 setCurrentUser((await res).data)
--- frontend/src/pages/register.jsx
+++ frontend/src/pages/register.jsx
@@ -20,7 +20,7 @@ const Register = () => {
       e.preventDefault();
   
       try {
-        const response = await axios.post("http://localhost:8800/api/auth/register", inputs);
+        const response = await axios.post("http://143.198.217.126:8800/api/auth/register", inputs);
     setErr(null); // Clear any previous errors
     console.log(response.data); // Access data if it exists
   } catch (error) {
